c831fffde8d2c21ac01b07a697263d98cc63c4d5
Prabhat Solanki <155136172+prabhats-github@users.noreply.github.com>
2024-02-10 15:45:05 +0530

    Add files via upload

---
+++ Enroll.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import st from './Enroll.module.css';
+
+const Enroll = () => {
+  return (
+    <div className={st.maindiv}>
+        <img src="https://static.javatpoint.com/images/training/software-testing-training1.png" alt="" />
+
+        <img src="https://static.javatpoint.com/images/training/digital-marketing-training1.png" alt="" />
+    </div>
+  )
+}
+
+export default Enroll
